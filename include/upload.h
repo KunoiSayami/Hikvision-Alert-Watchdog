49bed9158e9b324a3ab9eb4fa4f3f2d22e014174
KunoiSayami <46131041+KunoiSayami@users.noreply.github.com>
2020-08-10 18:04:51 +0800

    Add exportable datatype

--- include/upload.h
+++ include/upload.h
@@ -3,18 +3,26 @@
 #include <curl/curl.h>
 #include <nlohmann/json.hpp>
 #include <string>
-#include <chrono>
+//#include <chrono>
 #include <ctime>
+#include <nlohmann/json.hpp>
 
 namespace AlertWatchdog {
 	using std::string;
 	class Data {
-	private:
-		string data;
+	protected:
+		nlohmann::json data;
 		long long timestamp;
 	public:
 		Data();
-		Data(string);
+		//Data(string);
+		string GetJson() const;
+	};
+
+	class PostData : public Data {
+		const static unsigned long version = 0x1;
+	public:
+		PostData(unsigned long type, string message);
 	};
 
 	class UploadConfigure {
--- upload.cpp
+++ upload.cpp
@@ -1,9 +1,20 @@
-#include "include/upload.h"
+#include <upload.h>
 
 namespace AlertWatchdog {
 	UploadConfigure::UploadConfigure(string url) : remote_url(url) {}
 
-	Data::Data(): timestamp(time(nullptr)) {
+	Data::Data(): timestamp(time(nullptr)), data("{}") {
 		//auto obj = std::chrono::system_clock::now();
+	}
+
+	string Data::GetJson() const {
+		return this->data.dump(4);
+	}
+
+	PostData::PostData(unsigned long type, string message) : Data() {
+		this->data["version"] = version;
+		this->data["message"] = message;
+		this->data["type"] = type;
+		this->data["timestamp"] = timestamp;
 	}
 }
